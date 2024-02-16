26faeca293da18a4152f4bb50f03f1333b15220b
ecbrudner <148579054+ecbrudner@users.noreply.github.com>
2024-02-15 20:42:09 -0600

    made updates to server.js file

--- server.js
+++ server.js
@@ -20,21 +20,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
-
-const url = 'https://exercisedb.p.rapidapi.com/exercises?limit=10';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6e80b305f6mshb4c48ce28d66fabp1ee768jsn39199b79955f',
-		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
