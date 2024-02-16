61bb7111b63df72349e3522b05f83fd117c2182e
RLCastaldy <rlcastaldy@gmail.com>
2024-02-15 21:21:10 -0600

    commented out lines causing errors and added api call to homeroutes file

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,9 +2,31 @@ const path = require('path');
 const router = require('express').Router();
 
 
+const url = 'https://exercisedb.p.rapidapi.com/exercises?limit=10';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6e80b305f6mshb4c48ce28d66fabp1ee768jsn39199b79955f',
+		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+	}
+};
+
+async function apiCall(){
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  };
+  }
+
+
 
 router.get('/', (req, res) =>
-  console.log("Test")
+console.log("Test")
 );
+
+apiCall()
 
 module.exports=router
--- server.js
+++ server.js
@@ -2,20 +2,22 @@ const express = require('express');
 const Sequelize = require("sequelize");
 const path = require('path');
 const homeRoutes= require('./routes/homeRoutes.js');
-const exphbs = require('express-handlebars');
+// const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const app = express();
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// const hbs = exphbs.create({});
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/', homeRoutes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+// });
