0c248daca16d52a9b2d491d8eb6491da80816904
ecbrudner <148579054+ecbrudner@users.noreply.github.com>
2024-02-20 20:34:14 -0600

    added express-session to server.js file

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const Sequelize = require("sequelize");
 const path = require('path');
+const session = require('express-session');
 const homeRoutes= require('./routes/homeRoutes.js');
 const exphbs = require('express-handlebars');
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const User = require('./models/User');
+const Exercise = require('./models/Exercise');
+
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
+
+const sess = {
+  secret: 'Super secret',
+  cookie: {
+    maxAge: 3600000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -14,6 +39,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
 app.use('/', homeRoutes);
 
 sequelize.sync({ force: false }).then(() => {
