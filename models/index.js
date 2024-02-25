b46a5847e22b4f486945d57252e3386726f17d0a
RLCastaldy <rlcastaldy@gmail.com>
2024-02-25 11:30:43 -0600

    commented out variables not in use to get server to start correctly

--- models/index.js
+++ models/index.js
@@ -1,27 +1,27 @@
-const User = require('./User');
-const Exercise = require('./Exercise');
-const Favorites = require('./Favorites');
+// const User = require('./User');
+// const Exercise = require('./Exercise');
+// const Favorites = require('./Favorites');
 
 //many to many association between user and exercise
 
-User.belongsToMany(Exercise, {
-    through: {
-        model: 'Favorites',
-        unique: false
-    },
+// User.belongsToMany(Exercise, {
+//     through: {
+//         model: 'Favorites',
+//         unique: false
+//     },
 
-    as: 'favorite_exercises'
-});
+//     as: 'favorite_exercises'
+// });
 
-Exercise.belongsToMany(User, {
-    through: {
-        model: 'Favorites',
-        unique: false
-    },
+// Exercise.belongsToMany(User, {
+//     through: {
+//         model: 'Favorites',
+//         unique: false
+//     },
 
-    as: 'favorited_by'
-});
+//     as: 'favorited_by'
+// });
 
 
 
-module.exports = { User, Exercise, Favorites};
+// module.exports = { User, Exercise, Favorites};
